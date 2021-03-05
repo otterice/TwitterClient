03e832cd4637169a053c057c8baddda44b8074f6
otterice <josephdang101@gmail.com>
2021-03-04 17:16:19 -0800

    added character count and update twitterclient2 gif

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -24,6 +27,7 @@ public class ComposeActivity extends AppCompatActivity {
     public static final int MAX_TWEET_LENGTH = 140;
 
     EditText etCompose;
+    TextView tvCharacterCount;
     Button btnTweet;
 
     TwitterClient client;
@@ -37,6 +41,29 @@ public class ComposeActivity extends AppCompatActivity {
 
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
+
+        tvCharacterCount = findViewById(R.id.tvCharacterCount);
+
+        etCompose.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Fires right as the text is being changed (even supplies the range of text)
+                tvCharacterCount.setText(etCompose.getText().length() + "/140");
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // Fires right before text is changing
+                tvCharacterCount.setText(etCompose.getText().length() + "/140");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Fires right after the text has changed
+                tvCharacterCount.setText(etCompose.getText().length() + "/140");
+            }
+        });
 
         //Set click listener on button
         btnTweet.setOnClickListener(new View.OnClickListener() {
